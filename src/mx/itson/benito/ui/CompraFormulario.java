349b38eb8d30163878e6758872f66d69fb648a1e
PC <PC@DESKTOP-5BNAEOP>
2023-03-18 17:50:49 -0600

    Se realizaron cambios

--- src/mx/itson/benito/ui/CompraFormulario.java
+++ src/mx/itson/benito/ui/CompraFormulario.java
@@ -203,7 +203,7 @@ public class CompraFormulario extends javax.swing.JDialog {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         
-        
+       
         String folio  = txtFolio.getText();
         String fechaString = txtFecha.getText();
         String articulo = txtNombre.getText();
